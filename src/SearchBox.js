538c035a086617442c6a1f274678a2a3d96c6d36
AchilihuDaniel07 <achilihudaniel53@gmail.com>
2022-08-14 11:32:09 +0100

    neca-edit

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -7,7 +7,7 @@ function SearchBox(props) {
     <div className="search-box">
        <input id="search-text" onChange ={props.updateSearchInput} className='' type ='text' placeholder='Search...'/>
 
-       <a class="search" href="#">
+       <a class="search" href="https://wwww.facebook.com">
             <i class="fas fa-search"></i>
         </a>
     </div>
